3cc6aa600e71e4611decaf471a39837cfabbeb7f
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-16 00:51:16 +0600

    add files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-gray-600 text-4xl font-bold">This is Fuad</h1>
+    </div>
+  );
+
+};
+
+export default App;
